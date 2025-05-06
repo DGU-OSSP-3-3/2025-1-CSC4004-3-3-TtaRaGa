2e13f5f02085b21c041dcfca1fb108a629e6f77f
a1rhun <83606547+a1rhun@users.noreply.github.com>
2025-05-06 14:12:17 +0900

    [feat] 최초 커밋

---
+++ src/main/java/com/example/ttaraga/ttaraga/TtaragaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ttaraga.ttaraga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtaragaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtaragaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ttaraga/ttaraga/TtaragaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttaraga.ttaraga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtaragaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
