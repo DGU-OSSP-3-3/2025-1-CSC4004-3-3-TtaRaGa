f2e4d931d90eca4fb9fd6c7661c62bbdc7bf529a
MisteryKid <abade3548@gmail.com>
2025-05-16 13:48:51 +0900

    [feat] #12 test

---
+++ src/main/java/com/example/ttaraga/ttaraga/Service/RecommendedPointerService.java
@@ -0,0 +1,10 @@
+package com.example.ttaraga.ttaraga.Service;
+
+/*기능
+주어진 시간 내에 갈 수 있는 포인터를 필터링
+그중 하나를 고르는 로직 구현
+GraphHooper API를 호출하여 이동시간 계산 하는 부분 포함
+ */
+
+public class RecommendedPointerService {
+}
