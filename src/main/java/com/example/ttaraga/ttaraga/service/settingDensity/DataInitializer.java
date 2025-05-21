ca91b1c96016a57aea8f1e6232f13ad4e808b137
MisteryKid <abade3548@gmail.com>
2025-05-21 17:33:03 +0900

    [feat] #12 밀집도 관련 db구축완

--- src/main/java/com/example/ttaraga/ttaraga/service/settingDensity/DataInitializer.java
+++ src/main/java/com/example/ttaraga/ttaraga/service/settingDensity/DataInitializer.java
@@ -1,6 +1,6 @@
 package com.example.ttaraga.ttaraga.service.settingDensity;
 /*
-엑셀 데이터(인구밀집도 도시 이름 리스트) 를 db에 넣는 과정
+엑셀 데이터와 api의 밀집도 레벨(인구밀집도 관련 정보) 를 db에 넣는 과정
  */
 import com.example.ttaraga.ttaraga.entity.DensityAreaInfo;
 import lombok.RequiredArgsConstructor;
@@ -58,38 +58,3 @@ public class DataInitializer implements ApplicationRunner {
         System.out.println("--- 애플리케이션 초기화 완료 ---");
     }
 }
-//@Component
-//@RequiredArgsConstructor
-
-//public class DataInitializer {
-//    private final ExcelReaderService excelReaderService;
-//    private final DensityAreaInfoService areaInfoService;
-//
-//
-//
-//    @Bean
-//    public ApplicationRunner loadData() {
-//        System.out.println("엑셀 데이터 로드하기 ");
-//        return args -> {
-//            try (InputStream is = getClass().getResourceAsStream("C:\\DDDDD\\src\\main\\resources\\DensityAreaInfo.xlsx")) {
-//                if (is == null) {
-//                    throw new IllegalArgumentException("Excel 파일을 찾을 수 없습니다.");
-//                }
-//                // DB에 데이터가 없을 경우에만 저장
-//                if (areaInfoService.count() == 0) {
-//                    List<DensityAreaInfo> areaInfos = excelReaderService.readAreaInfoFromExcel(is);
-//                    areaInfoService.saveAll(areaInfos);
-//                    System.out.println("엑셀 데이터를 DB에 저장 완료, 총 개수: " + areaInfos.size());
-//                } else {
-//                    System.out.println("이미 데이터가 존재하므로 저장을 건너뜁니다.");
-//                }
-//
-//            } catch (Exception e) {
-//                System.err.println("엑셀 파일 로딩 중 오류 발생: " + e.getMessage());
-//                e.printStackTrace();
-//            }
-//        };
-//
-//    }
-//
-//}
