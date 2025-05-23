e6cd780c7124e10d6df20dee59738931db830b3a
jihoo <wlgn0432@gmail.com>
2025-05-23 15:42:10 +0900

    [feat]#13 코드 중복 수정

--- src/main/java/com/example/ttaraga/ttaraga/TtaragaApplication.java
+++ src/main/java/com/example/ttaraga/ttaraga/TtaragaApplication.java
@@ -1,20 +1,17 @@
 package com.example.ttaraga.ttaraga;
 
-import com.example.ttaraga.ttaraga.dto.BikeDto;
-import com.example.ttaraga.ttaraga.service.BikeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableScheduling // 스케줄링 활성화
 public class TtaragaApplication {
+
     public static void main(String[] args) {
         // Spring 애플리케이션 실행
         SpringApplication.run(TtaragaApplication.class, args);
     }
-
 
 }
